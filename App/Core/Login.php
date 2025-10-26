4e8227d1e8120cac334420fa4ec18fb5b2ab0c57
Zaynah Redzovic <zaynahredzovic@gmail.com>
2025-10-26 15:38:12 +0100

    Added sessions + turned view into OOP

--- App/Core/Login.php
+++ App/Core/Login.php
@@ -2,11 +2,12 @@
 namespace App\Core;
 use App\Controllers\LoginController;
 use App\Models\LoginModel;
-use LogicException;
+use App\Core\Sessions;
 
 class Login{
     private string $username;
     private string $password;
+    private string $user;
 
     public function __construct(){
         if($_SERVER["REQUEST_METHOD"]=="POST"){
@@ -18,53 +19,27 @@ class Login{
         }
     }
     public function checkUser() {
-        $errors = [];
-        
+        $errors= [];
         $loginModel = new LoginModel();
         if($loginModel->isInputEmpty($this->username, $this->password)){
-            $errors["empty_input"] = "Fill in all fields";
+            $errors["emptyInput"] = "Fill in all fields";
         }
 
-        $user = $loginModel->getUser($this->username, $this->password);
+        $this->user = $loginModel->getUser($this->username, $this->password);
         
         $loginController = new LoginController();
-        if($loginController->isUsernameWrong($user)){
-            $errors["empty_input"]= "Incorrect login info!";
+        if($loginController->isUsernameWrong($this->user)){
+            $errors["loginIncorrect"]= "Incorrect login info!";
         }
 
-        if(!$loginController->isUsernameWrong($user) && $loginController->isPasswordWrong($user)){
-            $errors["empty_input"]= "Incorrect login info!";
+        if(!$loginController->isUsernameWrong($this->user) && $loginController->isPasswordWrong($this->user)){
+            $errors["loginIncorrect"]= "Incorrect login info!";
         }
+
+        Sessions::sessionLoginErrors($errors);
+        Sessions::newLoginSession($this->user);
+
+        header("location: index.php?login=success");
     }
-
-    /* figure out where this goes
-    require_once "config_session.inc.php";
-
-        if($errors){
-            $_SESSION["error_login"] = $errors;
-
-            header("location: ../index.php");
-            die();
-        };
-
-        $newSessionId = session_create_id();
-        $sessionId = $newSessionId . "_" . $result;
-        session_id($sessionId);
-
-        $_SESSION["user_id"] = $result["id"];
-        $_SESSION["user_username"] = htmlspecialchars($result["username"]) ;
-
-        $_SESSION['last_regeneration'] = time();
-        
-        header("location: ../index.php?login=success");
-        $pdo = null;
-        $stmt = null;
-
-        die();
-
-    } catch (PDOException $e) {
-        die("Query failed: " . $e->getMessage());
-    }
-    */
 
 }
---
+++ App/Views/LoginView.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Views;
+
+
+use App\Core\Sessions;
+
+class LoginView{
+    private array $errors;
+
+    private function __construct(){
+        $errors = $this->errors;
+    }
+
+    public function checkLoginErrors(){
+        if(isset($_SESSION['errorLogin'])){
+            $errors = $_SESSION['errorLogin'];
+            
+            echo "<br>";
+
+            foreach ($errors as $error) {
+            echo '<p class="form-error>"'.$error."</p><br>";
+            }
+
+            unset($_SESSION['errorLogin']);
+        }elseif (isset($_GET['login']) && $_GET['login']==='success') {
+            echo "<br>";
+            echo '<p class="form-success"> Login success! </p>';
+    }
+    }
+}
